94fc14482337c2f07651d0c633d4a3980d7f5f7d
abdullahshaaban <abdullah.shaaban91@gmail.com>
2018-10-22 23:13:07 -0700

    test

--- starterbot.py
+++ starterbot.py
@@ -61,8 +61,8 @@ def handle_command(command, channel):
     if command.startswith("sprint notes"):
         response = "Sure .... here is the link for the sprint notes page: https://petcoalm.atlassian.net/wiki/spaces/PDWEB/pages/679510035/Petco.com+Sprint+Notes"
 
-    if command.startswith("flask"):
-        response = requests.get('http://127.0.0.1:5000/slack').text
+    #if command.startswith("flask"):
+        #response = requests.get('http://127.0.0.1:5000/slack').text
 
     # Sends the response back to the channel
     slack_client.api_call(
